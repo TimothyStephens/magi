6819c5154cc901153b4c056ef1660fac2a45bdb8
Timothy Stephens <ts942@sebs.rutgers.edu>
2023-11-05 15:54:10 -0500

    Test updates

--- workflow_2/tests/run_test.sh
+++ workflow_2/tests/run_test.sh
@@ -5,13 +5,13 @@ exec 1> >(tee "${0}.log.$(date +%s)") 2>&1
 
 #### Pre-run setup
 source ~/scripts/script_setup.sh
-set +eu; conda activate magi_2; set -eu
+set +eu; conda activate ~/miniconda3/envs/magi_2; set -eu
 export PYTHONPATH="/home/timothy/miniconda3/envs/magi_2/lib/python3.8/site-packages"
 
 FASTA="test_pep.fa"
 MZ="test_mz.txt"
 
 #### Start Script
-./../magi2.sh -f "$FASTA" -m "$MZ" --ncpus 2
+./../magi2.sh -f "$FASTA" -m "$MZ" --ncpus 2 --nparts 2
 
 
